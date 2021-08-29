e92c36170b48a580985b786055dd936b30139db4
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-08-29 10:19:40 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+export const Main = ({
+  title = 'NextJS Boilerplate',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+  </S.Wrapper>
+)
