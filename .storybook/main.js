87e4cd6302dd1541c5aa03bf99e19c99f462273d
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-08-29 19:38:12 -0300

    chore(storybook): resolve absolute paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/stories.tsx"
+  stories: [
+    '../src/components/**/stories.tsx'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
+  ],
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`)
+
+    return config
+  }
 }
