ba135920fac5e4795c42877f21e20c0c5be78bd5
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-07 14:45:18 -0300

    feat(menu): add Menu component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill'
+import {
+  ShoppingCart as ShoppingCartIcon,
+  Search as SearchIcon,
+  Close as CloseIcon
+} from '@styled-icons/material-outlined'
+
+import { Logo } from 'components/Logo'
+import { Button } from 'components/Button'
+import MediaMatch from 'components/MediaMatch'
+
+import * as S from './styles'
+
+export type MenuProps = {
+  username?: string
+}
+
+export const Menu = ({ username }: MenuProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MediaMatch lessThan="medium">
+          <MenuIcon aria-label="Open Menu" />
+        </MediaMatch>
+      </S.IconWrapper>
+
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
+
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+
+        <MediaMatch greaterThan="medium">
+          {!!username && <Button>Sign In</Button>}
+        </MediaMatch>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+
+          {!!username && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
+        </S.MenuNav>
+
+        {!username && (
+          <S.RegisterBox>
+            <Button fullWidth size="large">
+              Log in now
+            </Button>
+            <span>or</span>
+            <S.CreateAccount href="#" title="Sign Up">
+              Sign Up
+            </S.CreateAccount>
+          </S.RegisterBox>
+        )}
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
